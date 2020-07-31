caf265daeaf2e95acdb4004981e2e40df5025873
bettinacjohnson <65965766+bettinacjohnson@users.noreply.github.com>
2020-07-31 15:14:53 -0400

    added to models

--- finqueue/models.py
+++ finqueue/models.py
@@ -7,116 +7,116 @@ from django import forms
 
 # blank = false
 
-class UserRegistration(models.Model):
-    first_name = models.CharField(max_length=64, blank=False)
-    last_name = models.CharField(max_length=64, blank=False)
-    username = models.CharField(max_length=25, blank=False)
-    password = models.CharField(max_length=15, blank=False)
-    ethnicity = models.CharField(max_length= 40, blank=False)
-    gender = models.CharField(max_length=11, blank=False)
-    age = forms.IntegerField(blank=False)
-    email = models.CharField(max_length=125, blank=False)
-    education = models.CharField(max_length=45, blank=True)
-    disability = models.CharField(max_length=50, blank=True)
+# class UserRegistration(models.Model):
+#     first_name = models.CharField(max_length=64, blank=False)
+#     last_name = models.CharField(max_length=64, blank=False)
+#     username = models.CharField(max_length=25, blank=False)
+#     password = models.CharField(max_length=15, blank=False)
+#     ethnicity = models.CharField(max_length= 40, blank=False)
+#     gender = models.CharField(max_length=11, blank=False)
+#     age = forms.IntegerField(blank=False)
+#     email = models.CharField(max_length=125, blank=False)
+#     education = models.CharField(max_length=45, blank=True)
+#     disability = models.CharField(max_length=50, blank=True)
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
 
-# class AgeRange(models.Model):
-# 18-24
-# 25-34
-# 35-44
-# 45-54
-# 55-64
-# 65+
-def clean_age(self):
-    age = self.cleaned_data.get("age")  
-    if age < 18:
-        raise forms.ValidationError("You must be at least 18 years old to register for The Queue")
+# # class AgeRange(models.Model):
+# # 18-24
+# # 25-34
+# # 35-44
+# # 45-54
+# # 55-64
+# # 65+
+# def clean_age(self):
+#     age = self.cleaned_data.get("age")  
+#     if age < 18:
+#         raise forms.ValidationError("You must be at least 18 years old to register for The Queue")
 
-class Ethnicity(models.Model):
-    Black_AfricanAmerican = 'Black or African American'
-    White_Caucasian = 'White or Caucasian'
-    American_Indian_Alaskan_Native = 'American Indian or Alaskan Native'
-    Hispanic = 'Hispanic or Latino'
-    Multiracial = 'Multiracial' 
-    Other = 'Other'
-    Ethnicity_Choices = [
-        (Black_AfricanAmerican, 'Black'),
-        (White_Caucasian, 'White or Caucasian'),
-        (American_Indian_Alaskan_Native, 'American Indian or Alaskan Native'),
-        (Hispanic, 'Hispanic or Latino'),
-        (Multiracial, 'Multiracial'),
-        (Other, 'Other'),
-    ]
-ethnicity_choice = models.CharField(max_length=40, choices=Ethnicity_Choices)
+# class Ethnicity(models.Model):
+#     Black_AfricanAmerican = 'Black or African American'
+#     White_Caucasian = 'White or Caucasian'
+#     American_Indian_Alaskan_Native = 'American Indian or Alaskan Native'
+#     Hispanic = 'Hispanic or Latino'
+#     Multiracial = 'Multiracial' 
+#     Other = 'Other'
+#     Ethnicity_Choices = [
+#         (Black_AfricanAmerican, 'Black'),
+#         (White_Caucasian, 'White or Caucasian'),
+#         (American_Indian_Alaskan_Native, 'American Indian or Alaskan Native'),
+#         (Hispanic, 'Hispanic or Latino'),
+#         (Multiracial, 'Multiracial'),
+#         (Other, 'Other'),
+#     ]
+# ethnicity_choice = models.CharField(max_length=40, choices=Ethnicity_Choices)
 
-class GenderIdentity(models.Model):
-    Male = 'Male'
-    Female = 'Female'
-    Non_Binary = 'Non-Binary'
-    Transgender = 'Transgender'
-    Other = 'Other'
-    Gender_Identity = [
-        (Male, 'Male'),
-        (Female, 'Female'),
-        (Non_Binary, 'Non-Binary'),
-        (Other, 'Other'),
-    ]
-    gender_identity_options = models.CharField(max_length= 11 , choices = Gender_Identity, default=Other)
-
-
-class Sexual_Preference(models.Model):
-    Heterosexual = 'Heterosexual/Straight'
-    Bisexual = 'Bisexual'
-    Gay = 'Gay'
-    Lesbian = 'Lesbian'
-    Pansexual = 'Pansexual'
-    Asexual =  'Asexual'
-    Prefer = 'Prefer Not to Disclose'
-    sexual_preferences = [
-        (Heterosexual, 'Heterosexual or Straight'),
-        (Bisexual, 'Bisexual'),
-        (Gay, 'Gay'),
-        (Lesbian, 'Lesbian')
-        (Pansexual, 'Pansexual'),
-        (Asexual, 'Asexual'),
-        (Prefer, 'Prefer Not to Disclose'),
-    ]
-    sexual_preference_options = models.CharField(max_length=100, choices=sexual_preferences)
-
-class Education(models.Model):
-    Less_than_high_school_degree = 'Less than high school degree'
-    High_school_degree_equivalent = 'High school degree or equivalent (e.g. GED)'
-    No_degree = 'Some college but no degree'
-    Associate ='Associate degree'
-    Bachelor ='Bachelor degree'
-    Graduate = 'Graduate degree'
-    education_achieved = [
-        (Less_than_high_school_degree, 'Less than high school degree'),
-        (High_school_degree_equivalent, 'High school degree or equivalent (e.g. GED)'),
-        (No_degree, 'Some college but no degree'),
-        (Associate,'Associate degree'),
-        (Bachelor, 'Bachelor degree'),
-        (Graduate,'Graduate degree'),
-    ]
-    education_options = models.CharField(max_length=45, choices=education_achieved)
-
-class Disability(models.Model):
-    Yes = 'Yes, I have a disability'
-    No = 'No, I do not have a disability'
-    disability_options = models.CharField(max_length=50, blank=False)
+# class GenderIdentity(models.Model):
+#     Male = 'Male'
+#     Female = 'Female'
+#     Non_Binary = 'Non-Binary'
+#     Transgender = 'Transgender'
+#     Other = 'Other'
+#     Gender_Identity = [
+#         (Male, 'Male'),
+#         (Female, 'Female'),
+#         (Non_Binary, 'Non-Binary'),
+#         (Other, 'Other'),
+#     ]
+#     gender_identity_options = models.CharField(max_length= 11 , choices = Gender_Identity, default=Other)
 
 
-# TRUE_FALSE_CHOICES = (
-#     (True, 'Yes'),
-#     (False, 'No')
-# )
-# class MyModelForm(forms.ModelForm):
-#     class Meta:
-#         model = MyModel
-#         fields = ('attending',)
-#         widgets = {
-#             'attending': forms.Select(choices=TRUE_FALSE_CHOICES)
-#         }
+# class Sexual_Preference(models.Model):
+#     Heterosexual = 'Heterosexual/Straight'
+#     Bisexual = 'Bisexual'
+#     Gay = 'Gay'
+#     Lesbian = 'Lesbian'
+#     Pansexual = 'Pansexual'
+#     Asexual =  'Asexual'
+#     Prefer = 'Prefer Not to Disclose'
+#     sexual_preferences = [
+#         (Heterosexual, 'Heterosexual or Straight'),
+#         (Bisexual, 'Bisexual'),
+#         (Gay, 'Gay'),
+#         (Lesbian, 'Lesbian')
+#         (Pansexual, 'Pansexual'),
+#         (Asexual, 'Asexual'),
+#         (Prefer, 'Prefer Not to Disclose'),
+#     ]
+#     sexual_preference_options = models.CharField(max_length=100, choices=sexual_preferences)
+
+# class Education(models.Model):
+#     Less_than_high_school_degree = 'Less than high school degree'
+#     High_school_degree_equivalent = 'High school degree or equivalent (e.g. GED)'
+#     No_degree = 'Some college but no degree'
+#     Associate ='Associate degree'
+#     Bachelor ='Bachelor degree'
+#     Graduate = 'Graduate degree'
+#     education_achieved = [
+#         (Less_than_high_school_degree, 'Less than high school degree'),
+#         (High_school_degree_equivalent, 'High school degree or equivalent (e.g. GED)'),
+#         (No_degree, 'Some college but no degree'),
+#         (Associate,'Associate degree'),
+#         (Bachelor, 'Bachelor degree'),
+#         (Graduate,'Graduate degree'),
+#     ]
+#     education_options = models.CharField(max_length=45, choices=education_achieved)
+
+# class Disability(models.Model):
+#     Yes = 'Yes, I have a disability'
+#     No = 'No, I do not have a disability'
+#     disability_options = models.CharField(max_length=50, blank=False)
+# # something else is needed here...
+
+# # TRUE_FALSE_CHOICES = (
+# #     (True, 'Yes'),
+# #     (False, 'No')
+# # )
+# # class MyModelForm(forms.ModelForm):
+# #     class Meta:
+# #         model = MyModel
+# #         fields = ('attending',)
+# #         widgets = {
+# #             'attending': forms.Select(choices=TRUE_FALSE_CHOICES)
+# #         }
 
---
+++ users/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+
+# Create your models here.
+ 
+class UserRegistration(models.Model):
+    BLACK_AFRICANAMERICAN = 'Black or African American'
+    WHITE_CAUCASIAN = 'White or Caucasian'
+    AMERICAN_INDIAN_ALASKAN_NATIVE = 'American Indian or Alaskan Native'
+    HISPANIC = 'Hispanic or Latino'
+    MULTIRACIAL = 'Multiracial' 
+    OTHER = 'Other'
+    ETHNICITY_CHOICES = [
+        (BLACK_AFRICANAMERICAN, 'Black'),
+        (WHITE_CAUCASIAN, 'White or Caucasian'),
+        (AMERICAN_INDIAN_ALASKAN_NATIVE, 'American Indian or Alaskan Native'),
+        (HISPANIC, 'Hispanic or Latino'),
+        (MULTIRACIAL, 'Multiracial'),
+        (OTHER, 'Other')
+    ]
+    MALE = 'Male'
+    FEMALE= 'Female'
+    NON_BINARY = 'Non-Binary'
+    TRANSGENDER = 'Transgender'
+    OTHER = 'Other'
+    GENDER_IDENTITY = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (NON_BINARY, 'Non-Binary'),
+        (OTHER, 'Other'),
+    ]
+    HETEROSEXUAL = 'Heterosexual or Straight'
+    BISEXUAL = 'Bisexual'
+    GAY = 'Gay'
+    LESBIAN = 'Lesbian'
+    PANSEXUAL = 'Pansexual'
+    ASEXUAL =  'Asexual'
+    PREFER = 'Prefer Not to Disclose'
+    SEXUAL_PREFERENCES = [
+        (HETEROSEXUAL, 'Heterosexual or Straight'),
+        (BISEXUAL, 'Bisexual'),
+        (GAY, 'Gay'),
+        (LESBIAN, 'Lesbian')
+        (PANSEXUAL, 'Pansexual'),
+        (ASEXUAL, 'Asexual'),
+        (PREFER, 'Prefer Not to Disclose'),
+    ]
+    LESS_THAN_HIGH_SCHOOL_DEGREE = 'Less than high school degree'
+    HIGH_SCHOOL_DEGREE_EQUIVALENT = 'High school degree or equivalent (e.g. GED)'
+    NO_DEGREE = 'Some college but no degree'
+    ASSOCIATE ='Associate degree'
+    BACHELOR ='Bachelor degree'
+    GRADUATE = 'Graduate degree'
+    EDUCATION_ACHIEVED = [
+        (LESS_THAN_HIGH_SCHOOL_DEGREE, 'Less than high school degree'),
+        (HIGH_SCHOOL_DEGREE_EQUIVALENT, 'High school degree or equivalent (e.g. GED)'),
+        (NO_DEGREE, 'Some college but no degree'),
+        (ASSOCIATE,'Associate degree'),
+        (BACHELOR, 'Bachelor degree'),
+        (GRADUATE,'Graduate degree'),
+    ]
+    first_name = models.CharField(max_length=64, blank=False)
+    last_name = models.CharField(max_length=64, blank=False)
+    username = models.CharField(max_length=25, blank=False)
+    password = models.CharField(max_length=15, blank=False)
+    ethnicity = models.ChoiceField(max_length=40, blank=False, choices=ETHNICITY_CHOICES)
+    gender = models.CharField(max_length=11, blank=False, choices=GENDER_IDENTITY)
+    sexual_preference = models.CharField(max_length=50, blank=False, choices= SEXUAL_PREFERENCES)
+    age = models.IntegerField(blank=False)
+    email = models.CharField(max_length=125, blank=False)
+    education = models.CharField(max_length=45, blank=True, choices=EDUCATION_ACHIEVED)
+    disability = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.username
+
+# class AgeRange(models.Model):
+# 18-24
+# 25-34
+# 35-44
+# 45-54
+# 55-64
+# 65+
+
+class Disability(models.Model):
+    Yes = 'Yes, I have a disability'
+    No = 'No, I do not have a disability'
+    disability_options = models.CharField(max_length=50, blank=False)
+# something else is needed here...
+
+# TRUE_FALSE_CHOICES = (
+#     (True, 'Yes'),
+#     (False, 'No')
+# )
