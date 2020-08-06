d5915bbc31ed8b10c7aa685ba115de90c7a1dc32
Kieran Blanks <k.blanks12@gmail.com>
2020-08-06 10:22:06 -0500

    error

--- finqueue/models.py
+++ finqueue/models.py
@@ -1,13 +1,39 @@
 from django.db import models
 from django import forms
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Business(models.Model):
-    pass
+#Establishment categories 
+    HEALTHCARE = 'Healthcare'
+    RESTAURANTS = 'Restaurants'
+    SHOPPING = 'Shopping'
+    EDUCATION = 'Education'
+    BUSINESS_TYPE = [
+        (HEALTHCARE, 'Healthcare'),
+        (RESTAURANTS, 'Restaurants'),
+        (SHOPPING, 'Shopping'),
+        (EDUCATION, 'Education'),
+    ]
 
-class Reviews(models.Model):
-    pass
-#treat like new note 
+    Name = models.CharField(max_length=50)
+    Type = models.CharField(max_length=40, choices=BUSINESS_TYPE)
+    Location = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return str(self.Name)
+
+class Review(models.Model):
+    ethnicity = models.IntegerField()
+    age = models.IntegerField()
+    orientation = models.IntegerField()
+    gender = models.IntegerField()
+    disability = models.IntegerField()
+    education = models.IntegerField()
+    business = models.ForeignKey(to='Business',on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(to='User',on_delete=models.CASCADE, related_name='reviews')
+    timestamp = models.DateTimeField(auto_now_add=False)
+    note = models.TextField()
+
 
 
--- finqueue/views.py
+++ finqueue/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
